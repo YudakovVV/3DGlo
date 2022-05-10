cc9723fe4316433bc8e3d9770cd05642ef4bb486
YudakovVV <skiff2140@gmail.com>
2022-05-10 23:25:36 +0400

    edit lesson20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -6,25 +6,24 @@ const modal = () =>{
   
   const animate = ({timing, draw, duration}) => {
 
-    let start = performance.now();
+      let start = performance.now();
 
-    requestAnimationFrame(function animate(time) {
-    // timeFraction изменяется от 0 до 1
-    let timeFraction = (time - start) / duration;
-    if (timeFraction > 1) timeFraction = 1;
+      requestAnimationFrame(function animate(time) {
+      // timeFraction изменяется от 0 до 1
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) timeFraction = 1;
 
-    // вычисление текущего состояния анимации
-    let progress = timing(timeFraction);
+      // вычисление текущего состояния анимации
+      let progress = timing(timeFraction);
 
-    draw(progress); // отрисовать её
+      draw(progress); // отрисовать её
 
-    if (timeFraction < 1) {
-      requestAnimationFrame(animate);
-    }
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
 
-  });
-}
-
+    });
+  }
   
   buttons.forEach(btn =>{
     btn.addEventListener('click',()=>{
