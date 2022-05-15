e1c02614b15fb0a1b5ab843b15eb01d3292fa2e6
YudakovVV <skiff2140@gmail.com>
2022-05-15 23:04:44 +0400

    добавлено делегирование на клик

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -11,22 +11,40 @@ const menu = () => {
     //     else {
     //         menu.style.transform = ''
     //     }
-        menu.classList.toggle('active-menu')
+        menu.classList.toggle('active-menu')        
     }
     
+    
     menuBtn.addEventListener('click', handleMenu)
-    closeBtn.addEventListener('click', handleMenu)
+    // closeBtn.addEventListener('click', handleMenu)
 
-    // for(let i=0; i < menuItems.length; i++){
-    //     menuItems[i].addEventListener('click', handleMenu)
-    // }
-    menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu))
+    // // for(let i=0; i < menuItems.length; i++){
+    // //     menuItems[i].addEventListener('click', handleMenu)
+    // // }
+    // menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu))
 
-    // menu.addEventListener('click',(e)=>{
-    //     if(!e.target.closest('.menu') || e.target.classList.contains('close-btn')){
-    //   menu.style.display = 'none'
+    menu.addEventListener('click',(e)=>{
+        
+        if(!e.target.classList.contains('active-menu') )
+        {
+          menu.classList.toggle('active-menu')=== false
+        }
+        menuItems.forEach(menuItem => menuItem.classList.toggle('ul>li>a'))
+        // e.target.classList.contains('closeBtn')
+        // closeBtn.addEventListener('click', handleMenu)
+        // menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu))
+        // e.target.closest('.close-btn')
+        // for(let i=0; i < menuItems.length; i++){
+        // menuItems[i].matches('handleMenu')
     // }
-    // })
+    }
+    )
+
+//     modal.addEventListener('click',(e)=>{
+//     if(!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')){
+//       modal.style.display = 'none'
+//     }
+//   })
 }
 
 export default menu
