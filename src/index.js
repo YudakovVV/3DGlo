e7db2dfea772ce25551a42ae7317b46559a3f5aa
YudakovVV <skiff2140@gmail.com>
2022-05-26 23:10:34 +0400

    create lesson27

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import calculator from './modules/calculator'
 import forms from './modules/forms'
 import tabs from './modules/tabs'
 import slider from './modules/slider'
+import sendForm from './modules/sendForm'
 
 timer('30 may 2022')
 menu()
@@ -30,3 +31,10 @@ forms({
 })
 tabs()
 slider()
+sendForm({
+    formId:'form1', 
+    someElem: [{
+        type: 'block',
+        id: 'total'
+    }]
+})
